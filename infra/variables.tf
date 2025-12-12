756c50df12524baed213bdd333ec1b3cbcaee0ee
Faraz Najam <102857185+FarazNajam@users.noreply.github.com>
2025-12-12 12:35:20 +1100

    Remove unused Azure variables from variables.tf
    Removed multiple unused variables related to Azure Firewall and spoke virtual networks from the Terraform configuration.

--- infra/variables.tf
+++ infra/variables.tf
@@ -48,104 +48,16 @@ variable "hub_vnet_address_space" {
   default     = ["10.0.0.0/16"] 
 }
 
-variable "Azure_Firewall_Subnet" {
-  description = "Name of Firewall subnet."
-  type        = string
-  default     = "AzureFirewallSubnet"
-}
-
-variable "Azure_Firewall_Management_Subnet" {
-  description = "Name of Firewall Mgmt subnet."
-  type        = string
-  default     = "AzureFirewallManagementSubnet"
-}
-
-variable "AzureFirewallSubnet_address_prefix" {
-  type        = list(string)
-  default     = ["10.0.2.0/24"]
-}
-
-spokes = {
-  "vnet-spoke-app" = { address_space = ["10.1.0.0/16"] }
-  "vnet-spoke-db"  = { address_space = ["10.2.0.0/16"] }
-}
-
-
-
-
 ############################################################################
 
-variable "spoke_app_virtual_network_name" {
-  description = "Name of the spoke app VNet."
-  type        = string
-  default     = "vnet-spoke-app-prod"
-}
 
-variable "NSG_name" {
-  description = "Name of the NSG associated with subnet1."
-  type        = string
-}
-
-variable "hub_address_space" {
-  description = "Namae of the resource group where the Key Vault will be created."
-  type        = list(string)
-}
 
 #############################################################################
 
-variable "spoke_data_virtual_network_name" {
-  description = "Name of the spoke data VNet."
-  type        = string
-  default     = "vnet-spoke-data-prod"
-}
 
-variable "NSG_name" {
-  description = "Name of the NSG associated with subnet1."
-  type        = string
-}
-
-variable "subnet1" {
-  description = "Name of subnet1."
-  type        = string
-}
-
-variable "address_space" {
-  description = "Namae of the resource group where the Key Vault will be created."
-  type        = list(string)
-}
-
-variable "address_prefix" {
-  description = "Name of the resource group where the Key Vault will be created."
-  type        = list(string)
-}
 
 ##########################################################################
 
-variable "spoke_mgmt_virtual_network_name" {
-  description = "Name of the mgmt data VNet."
-  type        = string
-  default     = "vnet-spoke-mgmt-prod"
-}
-
-variable "NSG_name" {
-  description = "Name of the NSG associated with subnet1."
-  type        = string
-}
-
-variable "subnet1" {
-  description = "Name of subnet1."
-  type        = string
-}
-
-variable "address_space" {
-  description = "Namae of the resource group where the Key Vault will be created."
-  type        = list(string)
-}
-
-variable "address_prefix" {
-  description = "Name of the resource group where the Key Vault will be created."
-  type        = list(string)
-}
 
 ########################################################################
 
