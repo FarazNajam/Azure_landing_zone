758d05236c4b76a3e02c9ac71c0299fcd6d118e4
Faraz Najam <102857185+FarazNajam@users.noreply.github.com>
2026-01-01 02:36:50 +1100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -72,3 +72,34 @@ module "mgmt_spoke_virtual_network" {
   address_space        = var.mgmt_spoke_address_space
   subnets              = var.mgmt_spoke_subnets
 }
+
+#######################################################################################
+locals {
+  spokes = {
+    app  = { vnet_id = module.app_spoke_virtual_network.vnet_id,  vnet_name = module.app_spoke_virtual_network.vnet_name,  rg = module.app_spoke_virtual_network.resource_group_name }
+    data = { vnet_id = module.data_spoke_virtual_network.vnet_id, vnet_name = module.data_spoke_virtual_network.vnet_name, rg = module.data_spoke_virtual_network.resource_group_name }
+    mgmt = { vnet_id = module.mgmt_spoke_virtual_network.vnet_id, vnet_name = module.mgmt_spoke_virtual_network.vnet_name, rg = module.mgmt_spoke_virtual_network.resource_group_name }
+  }
+}
+
+module "peer_hub_to_spokes" {
+  for_each = local.spokes
+  source   = "./modules/Vnet_peering"
+
+  name                      = "peer-hub-to-${each.key}-spoke"
+  resource_group_name       = module.hub_virtual_network.resource_group_name
+  virtual_network_name      = module.hub_virtual_network.vnet_name
+  remote_virtual_network_id = each.value.vnet_id
+}
+
+module "peer_spokes_to_hub" {
+  for_each = local.spokes
+  source   = "./modules/Vnet_peering"
+
+  name                      = "peer-${each.key}-spoke-to-hub"
+  resource_group_name       = each.value.rg
+  virtual_network_name      = each.value.vnet_name
+  remote_virtual_network_id = module.hub_virtual_network.vnet_id
+}
+
+
