d0483cba7da60665be39a02913eb907db4491f78
Faraz Najam <102857185+FarazNajam@users.noreply.github.com>
2026-01-01 17:19:16 +1100

    Create variables.tf

---
+++ infra/modules/Virtual_Machines/variables.tf
@@ -0,0 +1,22 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Existing RG to deploy into"
+}
+
+variable "location" {
+  type        = string
+  default     = "australiaeast"
+  description = "Location; defaults to RG location if null"
+}
+
+variable "nic" {
+  type    = string
+}
+
+variable "VM_name" {
+  type = string
+}
+
+variable "VM_size" {
+  type = string
+}
